786dd8a7a5f533ffe988dd8a44cb5333feac4ee1
Matthias Koenig <konigmatt@googlemail.com>
2022-08-31 13:33:52 +0200

    updated code and todos

--- README.md
+++ README.md
@@ -1,12 +1,37 @@
 # Zonation analysis
 
-Analysis of zonation patterns from fluorscence images.
+Analysis of zonation patterns from fluorscence images. Double stainings with CYP2E1 and E-Cadharin were used to determine the zonation pattern of the liver
 
-## Read images
-- [ ] Read all images with bioformats
-- [ ] Assign channels
-- [ ] Store data
+## Normalization of channels
+- [ ] updated histogram normalization. In the first version a simple quantile cutoff is used. Better approaches are to determine the background values and which to remove.
+- how should the removed data be handled (mean filter?, NaNs)
 
-## Extract channels
+## Gauss filtering
+- [ ] adapt the gauss filter according to the spatial resolution of the system. I.e. the resolution parameter of the system must be used to adapt the filter size.
 
-## Normalize channels
+## Zonation score
+- Adjust zonation score between [0, 1] with actual meaning in the colorbar (pp=0, pv=1)
+- How to handle absolute quantification between the different slides ?!
+
+## Background & Vene segmentation/detection
+- necessary to detect the background
+
+
+## Analysis of patterns
+- [ ] create filter for zones based on thresholds (multiple thresholds), i.e., use 10 areas in histogram and map the data on it
+- [ ] determine DAPI/Zone or nucleus statistics per zone; use this as an example to match additional channel data on the zonation patterns
+- determine the shortest paths between minima and maxima (gradient paths), and calculate statistics along the path lines.
+
+## Store analysis results
+- [ ] write results as images (TIFF) which can be read again with standard software such as QuPath (add additional layers for). This includes the zonation patterns, but also
+
+## Interactive exploration
+- [ ] use software/tools for the interactive exploration of channels/blending. QuPath is already very nice, but an additional transparancy option would be required
+
+## Additional data
+- [ ] glycogen scans
+- [ ] Whole-slide scans Uta (Mohammed), check what else will be done in the project.
+
+## Better measurements (Scans)
+- [ ] perform test whole-slide scans
+- [ ] perform example scan on the Fluorescence Microscope (ITB)
---
+++ src/zonation/analyse_vessels_backgroud.py
@@ -0,0 +1,85 @@
+"""Detect vessels and background from the slides."""
+import numpy as np
+from zonation import IMAGE_PATH
+from zonation.analyse_zonation import histogram_normalization
+from zonation.plots import plot_image_with_hist
+from zonation.read_images import FluorescenceImage, Fluorophor
+
+
+def run_vessel_detection(sid: str, show_plot: bool = True):
+    """Run image analysis for given image sid."""
+    print(f"--- {sid} ---")
+    fimage = FluorescenceImage.from_file(IMAGE_PATH / f"{sid}.pickle")
+
+    # TODO: check the assignments of proteins to the fluorophors
+    cyp2e1 = fimage.get_channel_data(Fluorophor.ALEXA_FLUOR_488)
+    ecad = fimage.get_channel_data(Fluorophor.CY3)
+    dapi = fimage.get_channel_data(Fluorophor.DAPI)
+
+    # Display raw data of channel
+    if dapi is not None:
+        image_raw = np.zeros(shape=(cyp2e1.shape[0], cyp2e1.shape[1], 3))
+        image_raw[:, :, 2] = dapi
+    else:
+        image_raw = np.zeros(shape=(cyp2e1.shape[0], cyp2e1.shape[1], 2))
+    image_raw[:, :, 0] = cyp2e1
+    image_raw[:, :, 1] = ecad
+
+    plot_image_with_hist(image_raw, cmap="gray", title=f"{sid}: raw data",
+                         path=RESULTS_PATH / f"raw_{sid}.png")
+
+    # perform histogram normalization
+    image_hist = histogram_normalization(image_raw)
+    plot_image_with_hist(image_hist, cmap="gray", title=f"{sid}: histogram processing", path=RESULTS_PATH / f"hist_{sid}.png")
+
+    # calculate and plot ratios
+    cmap = "seismic"
+    plot_zonation(image_hist, cmap=cmap, title=f"{sid}: difference and ratio", path=RESULTS_PATH / f"diffratio_{sid}.png")
+
+    # gauss normalization (depending on radius/structure)
+    image_gauss = gaussian(image_hist, sigma=20, multichannel=True, channel_axis=None)
+    # image_gauss = gaussian(image_gauss, sigma=10, channel_axis=1)
+    plot_zonation(image_gauss, cmap=cmap, title=f"{sid}: gauss filtering for averaging", path=RESULTS_PATH / f"gauss_{sid}.png")
+
+    # calculate zonation measures & plot the overlays
+    plot_overlay(
+        image_hist, image_gauss,
+        zonation=image_hist[:, :, 0] - image_hist[:, :, 1],
+        zonation_gauss=image_gauss[:, :, 0] - image_gauss[:, :, 1],
+        cmap=cmap, alpha=0.4,
+        title=f"{sid}: Zonation Difference (CYP2E1 - E-Cadherin)",
+        path=RESULTS_PATH / f"overlay_difference_{sid}.png"
+    )
+    plot_overlay(
+        image_hist, image_gauss,
+        zonation=image_hist[:, :, 0] / image_hist[:, :, 1],
+        zonation_gauss=image_gauss[:, :, 0] / image_gauss[:, :, 1],
+        cmap=cmap, alpha=0.4,
+        title=f"{sid}: Zonation Ratio (CYP2E1 / E-Cadherin)",
+        path=RESULTS_PATH / f"overlay_ratio_{sid}.png"
+    )
+
+    # TODO: gauss filter based on geometry (resolution), i.e. different meaning depending on resolution
+    # TODO: area quantification via hist (20 areas), i.e. a distribution
+    # TODO: store results in image for visualization
+
+    # Display raw data of channel
+    image_out = np.zeros(shape=(cyp2e1.shape[0], cyp2e1.shape[1], 5))
+    image_out[:, :,0] = image_hist[:, :, 0]
+    image_out[:, :, 1] = image_hist[:, :, 1]
+    if dapi is not None:
+        image_out[:, :, 2] = image_hist[:, :, 2]
+    # zonation difference
+    image_out[:, :, 3] = image_hist[:, :, 0] - image_hist[:, :, 1]
+    image_out[:, :, 4] = image_hist[:, :, 0] - image_hist[:, :, 1]
+
+    # TODO: save image as tif with zonation channels
+
+
+if __name__ == "__main__":
+
+    # for p in CZI_IMAGES:
+    #    sid = p.stem
+    #    run_analysis(sid)
+
+    run_analysis("Test33")
