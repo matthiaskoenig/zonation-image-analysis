0f3d78c7b750b59857fb433bb63e4898e848cca6
jonaspk98 <jonas.kuettner98@gmail.com>
2023-05-22 19:19:47 +0200

    some experimantation with qpath annotations and openslide, zarr, shapely.

---
+++ src/zia/annotations/geojson_and_shapely.py
@@ -0,0 +1,42 @@
+import geojson as gj
+import numpy as np
+from shapely.geometry import shape, Polygon, MultiPolygon
+from PIL import Image, ImageDraw
+import matplotlib.pyplot as plt
+import napari
+import openslide
+import zarr
+
+PATH_TO_FILE = "/home/jkuettner/Development/qpath/J-12-00350_NOR-022_Lewis_CYP2E1- 1 300_Run 14_ML, Sb, rk_MAA_006.geojson"
+PATH_TO_ZARR = "/home/jkuettner/Pictures/zarr/img2.zarr"
+if __name__ == "__main__":
+    with open(PATH_TO_FILE) as f:
+        data = gj.load(f)
+
+    # extracting features from the dict
+    features = data["features"]
+
+    geometries = [shape(feature["geometry"]) for feature in features]
+
+    mask = Image.new("L", (71424, 83456), 0)
+
+    draw = ImageDraw.Draw(mask)
+    for geometry in geometries:
+        if isinstance(geometry, Polygon):
+            coords = list(geometry.exterior.coords)
+            draw.polygon(coords, fill=255)
+        elif isinstance(geometry, MultiPolygon):
+            for poly in geometry.geoms:
+                coords = list(poly.exterior.coords)
+                draw.polygon(coords, fill=255)
+
+    mask_array = np.array(mask)
+
+    zarray = zarr.array(mask_array,
+                        store=PATH_TO_ZARR)
+
+    # zarr.convenience.save_array(PATH_TO_ZARR, mask_array)
+
+    # viewer = napari.Viewer()
+    # viewer.add_image(mask_array, name="mask")
+    # napari.run()
---
+++ src/zia/annotations/openslide_example.py
@@ -0,0 +1,26 @@
+import matplotlib.cm
+import numpy as np
+import openslide
+import zarr
+from matplotlib import pyplot as plt, cm
+
+PATH_TO_PIC = "/home/jkuettner/Pictures/wsi/J-12-00350_NOR-022_Lewis_CYP2E1- 1 300_Run 14_ML, Sb, rk_MAA_006.ndpi"
+PATH_TO_ZARR = "/home/jkuettner/Pictures/zarr/img2.zarr"
+
+if __name__ == "__main__":
+    image = openslide.OpenSlide(PATH_TO_PIC)
+    mask = zarr.open(store=PATH_TO_ZARR, mode="r+")
+
+    x = 54064 - 256
+    y = 50547 - 256
+
+    region = image.read_region((x, y), 0, (512, 512))
+
+    selection = mask.get_basic_selection((slice(y, y + 512), slice(x, x + 512)))
+    print(np.max(selection))
+    selection = np.ma.masked_where(selection == 0, selection)
+    fig, ax = plt.subplots(1, 1)
+    ax: plt.Axes
+    ax.imshow(region)
+    ax.imshow(selection, cmap=matplotlib.cm.get_cmap("jet"))
+    plt.show()
