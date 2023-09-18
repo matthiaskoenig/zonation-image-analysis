9238a99ff6d3c0b7b8708e0e928ae11d99f27b15
jonaspk98 <jonas.kuettner98@gmail.com>
2023-09-18 13:10:34 +0200

    cleaned code up and added doc strings

--- src/zia/processing/get_segments.py
+++ src/zia/processing/get_segments.py
@@ -11,11 +11,17 @@ class LineSegmentsFinder:
         self.segments_to_do = []
         self.nodes = []
 
-    def get_neighbors(self, pixel: Tuple[int, int], tight=True):
+    def get_neighbors(self, pixel: Tuple[int, int], ortho=True):
+        """
+        gets the neighboring pixels for a pixel
+        @param pixel: central pixel
+        @param ortho: if True orthogonal neighbors are returned, else diagonal
+        @return: neighboring pixels
+        """
         neighbors = []
         h, w = pixel
         ih, iw = self.image_shape
-        directions = [(-1, 0), (0, -1), (0, 1), (1, 0)] if tight else [(-1, -1), (-1, 1), (1, -1), (1, 1)]
+        directions = [(-1, 0), (0, -1), (0, 1), (1, 0)] if ortho else [(-1, -1), (-1, 1), (1, -1), (1, 1)]
 
         for dh, dw in directions:
             nh, nw = h + dh, w + dw
@@ -24,6 +30,12 @@ class LineSegmentsFinder:
         return neighbors
 
     def get_connected_pixels(self, neighbors: List[Tuple[int, int]], remove=True) -> List[Tuple[int, int]]:
+        """
+        finds the connected pixels by checking if the given neighboring pixel is in the pixels list.
+        @param neighbors: the neighboring pixels
+        @param remove: if True, the found pixels are removed from the pixels list
+        @return: list of found connected pixels.
+        """
         n_pixels = [n for n in neighbors if n in self.pixels]
         if remove:
             for n_pixel in n_pixels:
@@ -31,10 +43,13 @@ class LineSegmentsFinder:
         return n_pixels
 
     def get_loop_end(self, neighbors: List[Tuple[int, int]]) -> List[List[Tuple[int, int]]]:
+        """
+        finds the connected segments by checking if the neighboring pixel is equal to the last pixel
+        of the segments in the segments to do list
+        @param neighbors: neighboring pixels, which might be the end of another segment
+        @return: list of the found connected segments
+        """
         return list(filter(lambda s: any([s[-1] == n for n in neighbors]), self.segments_to_do))
-
-    def get_diag_loop_end(self, neighbors: List[Tuple[int, int]], this_pixel: Tuple[int, int]) -> List[List[Tuple[int, int]]]:
-        return list(filter(lambda s: any([(s[0] == n and s[1] == this_pixel) for n in neighbors]), self.segments_to_do))
 
     def walk_segment(self, segment: List[tuple[int, int]]) -> None:
         """
@@ -43,7 +58,7 @@ class LineSegmentsFinder:
         print("walk segment")
         this_pixel = segment[-1]
         n_ortho = self.get_neighbors(this_pixel)
-        n_diagonal = self.get_neighbors(this_pixel, tight=False)
+        n_diagonal = self.get_neighbors(this_pixel, ortho=False)
 
         # remove already visited from neighbors
         if segment[-2] in n_ortho:
@@ -67,7 +82,7 @@ class LineSegmentsFinder:
             # find the diagonal neighbor that is far from the orthogonally connected pixel
             potential_branch = self.get_potential_branching_pixel(segment[-2], connected_pixel, n_diagonal)
 
-            # if that neighbor exists, check if it is another pixel or another segement end
+            # if that neighbor exists, check if it is another pixel or another segment end
             if potential_branch is not None:
                 if potential_branch in self.pixels:
                     orthogonally_connected.append(potential_branch)
@@ -78,7 +93,7 @@ class LineSegmentsFinder:
 
         # if one entry exists, extend the segment otherwise check for diagonal pixels
         if len(orthogonally_connected) > 0 or len(ortho_connected_segments) > 0:
-            self.check_diag_segements(ortho_connected_segments, this_pixel)
+            self.check_connected_segments(ortho_connected_segments, this_pixel)
             self.extend_segment(this_pixel, segment, orthogonally_connected, ortho_connected_segments)
             return
 
@@ -87,16 +102,22 @@ class LineSegmentsFinder:
         diagonally_connected = self.get_connected_pixels(n_diagonal)
         connected_segments_diag = self.get_simple_connected_segments(n_diagonal, diagonally_connected)
         connected_segments_diag = self.filter_connected_segments(connected_segments_diag, segment)
-        self.check_diag_segements(connected_segments_diag, this_pixel)
+        self.check_connected_segments(connected_segments_diag, this_pixel)
 
-        self.extend_segment(this_pixel, segment, diagonally_connected, connected_segments_diag, diag=True)
+        self.extend_segment(this_pixel, segment, diagonally_connected, connected_segments_diag)
         return
 
-    def check_diag_segements(self, connected_segments_diag: List[List[Tuple[int, int]]], orig_pixel):
-        for connected_segment in connected_segments_diag:
+    def check_connected_segments(self, connected_segments: List[List[Tuple[int, int]]], orig_pixel) -> None:
+        """
+        this method is need to check if encountered segments have neighbors. These would otherwise get lost
+        because the segment is removed from the segments to do list.
+        @param connected_segments: list of connected segments
+        @param orig_pixel: the pixel of which the neighbor is adjacent to the connected segments
+        """
+        for connected_segment in connected_segments:
             this_pixel = connected_segment[-1]
             n_ortho = self.get_neighbors(this_pixel)
-            n_diagonal = self.get_neighbors(this_pixel, tight=False)
+            n_diagonal = self.get_neighbors(this_pixel, ortho=False)
 
             # remove already visited from neighbors
             if orig_pixel in n_ortho:
@@ -127,16 +148,15 @@ class LineSegmentsFinder:
                     self.segments_to_do.append([connected_segment[-1], con_pixel])
                 continue
 
-            connected_segments_diag = self.get_simple_connected_segments(n_diagonal, diag_connected)
-            if len(connected_segments_diag) != 0:
-                for con_seg in connected_segments_diag:
+            connected_segments = self.get_simple_connected_segments(n_diagonal, diag_connected)
+            if len(connected_segments) != 0:
+                for con_seg in connected_segments:
                     self.segments_to_do.append([connected_segment[-1], con_seg[-1]])
                 continue
-    def get_diagonally_branching_pixel(self, ortho: Tuple[int, int], diag: List[Tuple[int, int]]) -> Optional[Tuple[int, int]]:
-        branches = list(filter(lambda d: self.is_branch(ortho, d), diag))
-        if len(branches) == 1:
-            return branches[0]
-        return None
+    def get_diagonally_branching_pixel(self, ortho: Tuple[int, int], diag: List[Tuple[int, int]]) -> List[Tuple[int, int]]:
+        return list(filter(lambda d: self.is_branch(ortho, d), diag))
+
+
 
     def get_potential_branching_pixel(self, origin: Tuple[int, int], ortho: Tuple[int, int], diag: List[Tuple[int, int]]) -> Optional[
         Tuple[int, int]]:
@@ -183,6 +203,14 @@ class LineSegmentsFinder:
                                neighbors_ortho: List[Tuple[int, int]],
                                neighbors_diag: List[Tuple[int, int]],
                                connected_pixels: List[Tuple[int, int]]) -> List[List[Tuple[int, int]]]:
+        """
+        Gets all the orthogonally connected segments and diagonally connected segments which are not adjacent to
+        any of the orthogonally connected segments.
+        @param neighbors_ortho: orthogonally connected neighboring pixels
+        @param neighbors_diag: diagonally connected neighboring pixels
+        @param connected_pixels: actual connected pixels.
+        @return: list of connected segments
+        """
         branching_n_diag = self.get_branching_neighbors(neighbors_diag, connected_pixels)
         # get the potential branching diagonal neighbors
         remaining_n = list(filter(lambda x: x not in connected_pixels, neighbors_ortho + branching_n_diag))
@@ -194,19 +222,19 @@ class LineSegmentsFinder:
         remaining_n = list(filter(lambda x: x not in connected_pixels, neighbors))
         return self.get_loop_end(remaining_n)
 
-    def get_diag_connected_segments(self,
-                                    neighbors: List[Tuple[int, int]],
-                                    connected_pixels: List[Tuple[int, int]],
-                                    this_pixel: Tuple[int, int]) -> List[List[Tuple[int, int]]]:
-        remaining_n = list(filter(lambda x: x not in connected_pixels, neighbors))
-        return self.get_diag_loop_end(remaining_n, this_pixel)
-
     def extend_segment(self,
                        this_pixel: Tuple[int, int],
                        segment: List[Tuple[int, int]],
                        connected_pixels: List[Tuple[int, int]],
-                       connected_segments: List[List[Tuple[int, int]]],
-                       diag=False) -> None:
+                       connected_segments: List[List[Tuple[int, int]]]) -> None:
+        """
+        Extends or finishes the current segment based on the connected pixels and the connected segments
+        @param this_pixel: the current pixel
+        @param segment: the current segment
+        @param connected_pixels: pixels connected to current pixel
+        @param connected_segments: segments connected to current pixel
+        @return: None
+        """
         # if no connection found -> branch ending in the void
         if len(connected_segments) == 0:
             self.extend_segment_pixels_only(this_pixel, segment, connected_pixels)
@@ -240,6 +268,13 @@ class LineSegmentsFinder:
                                    this_pixel: Tuple[int, int],
                                    segment: List[Tuple[int, int]],
                                    connected_pixels: List[Tuple[int, int]]) -> None:
+        """
+        Extends or finished the current segment with based on the connected pixels.
+        @param this_pixel: the current pixel
+        @param segment: the current segment
+        @param connected_pixels: the pixels connected to the current pixel
+        @return: None
+        """
         if len(connected_pixels) == 0:
             self.segments_finished.append(segment)
             return
@@ -260,15 +295,23 @@ class LineSegmentsFinder:
             return
 
     def process_segments(self):
+        """
+        Processes the segments in the segments to do list until as this list is not empty.
+        """
         print("process segments")
         while len(self.segments_to_do) != 0:
             next_segment = self.segments_to_do.pop()
             print("new segment")
             self.walk_segment(next_segment)
 
-    def initialize(self, this_pixel: Tuple[int, int]):
+    def initialize(self, this_pixel: Tuple[int, int]) -> None:
+        """
+        initializes the segmentation algorithm. It takes the given pixel and finds connected pixel to
+        initialize the segments to do list
+        @param this_pixel: the first pixel.
+        """
         n_ortho = self.get_neighbors(this_pixel)
-        n_diagonal = self.get_neighbors(this_pixel, tight=False)
+        n_diagonal = self.get_neighbors(this_pixel, ortho=False)
 
         # get connected pixels
         orthogonally_connected = self.get_connected_pixels(n_ortho)
@@ -277,10 +320,11 @@ class LineSegmentsFinder:
 
             if len(orthogonally_connected) == 1:
                 diagonally_connected = self.get_connected_pixels(n_diagonal, False)
-                branch = self.get_diagonally_branching_pixel(orthogonally_connected[0], diagonally_connected)
-                if branch is not None:
-                    self.pixels.remove(branch)
-                    orthogonally_connected.append(branch)
+                branches = self.get_diagonally_branching_pixel(orthogonally_connected[0], diagonally_connected)
+                if len(branches) != 0:
+                    for branch in branches:
+                        self.pixels.remove(branch)
+                        orthogonally_connected.append(branch)
 
             for p in orthogonally_connected:
                 self.segments_to_do.append([this_pixel, p])
@@ -299,7 +343,9 @@ class LineSegmentsFinder:
         return
 
     def run(self) -> None:
-        # find a pixel with a neighbor that's not been processed yet
+        """
+        Runs the line segmentation.
+        """
         print("run segmentation")
         while len(self.pixels) > 0:
             pixel = self.pixels.pop()
@@ -308,9 +354,17 @@ class LineSegmentsFinder:
 
     def filter_connected_segments(self, connected_segments: List[List[Tuple[int, int]]], segment: List[Tuple[int, int]]) -> List[
         List[Tuple[int, int]]]:
-        return [s for s in connected_segments if len(segment) > 2 or (s[0] != segment[0] and not self.is_ortho(s, segment))]
+        """
+        Filters connected segments to avoid small circles:
+        Case 1: Two segments of length 2 that have the same origin.
+        Case 2: Two parallely aligned segments of length 2. This happens if there are 4 pixels in a square layout.
+        @param connected_segments: the connected segments to check
+        @param segment: the current segment in progress
+        @return: the filtered list of valid segments
+        """
+        return [s for s in connected_segments if len(segment) > 2 or (s[0] != segment[0] and not self.is_ortho_and_aligned(s, segment))]
 
-    def is_ortho(self, s1, s2) -> bool:
+    def is_ortho_and_aligned(self, s1, s2) -> bool:
         v1 = np.array([s1[1][0] - s1[0][0], s1[1][1] - s1[0][1]])
         v2 = np.array([s2[1][0] - s2[0][0], s2[1][1] - s2[0][1]])
 
@@ -321,6 +375,12 @@ class LineSegmentsFinder:
         return False
 
     def get_branching_neighbors(self, neighbors_diag: List[Tuple[int, int]], connected_pixels: List[Tuple[int, int]]) -> List[Tuple[int, int]]:
+        """
+        Finds diagonally branching neighbors
+        @param neighbors_diag: diagonally neighboring pixels
+        @param connected_pixels: orthogonally connected pixels
+        @return: list of neighbors. Max length of list is one.
+        """
         if len(connected_pixels) == 0 or len(connected_pixels) > 1:
             return []
         potential_branches = [n for n in neighbors_diag if self.is_branch(connected_pixels[0], n)]
