8ed4ee95b4e1e4589b7d0ceaba69113c8e4a4089
jonaspk98 <jonas.kuettner98@gmail.com>
2023-10-12 17:06:26 +0200

    fractal dimension

--- src/zia/processing/fractal_dimension.py
+++ src/zia/processing/fractal_dimension.py
@@ -6,6 +6,13 @@ from zia.config import read_config
 from zia.processing.lobulus_statistics import SlideStats
 from zia.processing.visualization_species_comparison import merge_to_one_df
 
+from scipy.optimize import curve_fit
+
+
+def fractal(p, d, k):
+    return 2 / d * (p - k)
+
+
 if __name__ == "__main__":
     config = read_config(BASE_PATH / "configuration.ini")
     data_dir_stain_separated = config.image_data_path / "slide_statistics"
@@ -52,15 +59,26 @@ if __name__ == "__main__":
         marker_iter = iter(markers)
 
         for subject, subject_df in species_df.groupby("subject"):
-
-            ax.scatter(np.log10(species_df["perimeter"]),
-                       np.log10(species_df["area"]),
+            ax.scatter(subject_df["perimeter"],
+                       subject_df["area"],
                        color=color,
                        alpha=0.4,
                        marker=next(marker_iter)
                        )
+    x, y = np.log10(df["perimeter"]), np.log10(df["area"])
+    pop, pcov = curve_fit(fractal, x, y)
 
+    x_plot = np.linspace(np.min(x), np.max(x), 50)
 
+    y_plot = fractal(x_plot, pop[0], pop[1])
 
+    ax.plot(10 ** x_plot, 10 ** y_plot, color="black", marker="none")
+
+    ax.set_xscale("log")
+    ax.set_yscale("log")
+
+    ax.set_ylabel("area (µm$^2$)")
+    ax.set_xlabel("perimeter (µm)")
+    print(pop)
 
     plt.show()
