46347b808d4cf042e762c5d15bdf4086682f1182
Matthias König <konigmatt@googlemail.com>
2023-10-19 18:33:28 +0200

    work on spatial PCA

--- spatialPCA/installation.R
+++ spatialPCA/installation.R
@@ -9,5 +9,18 @@ if (!requireNamespace("devtools", quietly = TRUE))
     install.packages("devtools")
 # Install SpatialPCA
 devtools::install_github("shangll123/SpatialPCA")
+install.packages("peakRAM")
+install.packages("Matrix")
+
+
+# Wraps common clustering algorithms in an easily extended S4 framework. 
+# Backends are implemented for hierarchical, k-means and graph-based clustering. 
+# Several utilities are also provided to compare and evaluate clustering results.
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("bluster")
+
 # load SpatialPCA
 library(SpatialPCA)
+library(peakRAM)
--- spatialPCA/slideseq_cerebrellum.R
+++ spatialPCA/slideseq_cerebrellum.R
@@ -1,5 +1,8 @@
 # https://lulushang.org/SpatialPCA_Tutorial/slideseq.html
+# https://lulushang.org/docs/Projects/SpatialPCA/Slideseq
 
+library(SpatialPCA)
+library(peakRAM)
 
 rm(list = ls())
 setwd("/home/mkoenig/git/zonation-image-analysis/spatialPCA")
@@ -9,3 +12,86 @@ print(dim(sp_count)) # The count matrix:  (proteins, locations)
 print(dim(location)) # The location matrix: (x,y), locations
 
 
+# location matrix: n x 2, count matrix: g x n.
+# here n is spot number, g is gene number.
+# here the column names of sp_count and rownames of location should be matched
+# slideseq = CreateSpatialPCAObject(counts=sp_count, location=location, project = "SpatialPCA",gene.type="spatial",sparkversion="sparkx",numCores_spark=5, customGenelist=NULL,min.loctions = 20, min.features=20)
+
+# -----------------
+# Run spatialPCA
+# -----------------
+mem_sparse1 <- peakRAM({
+  start_time <- Sys.time()
+  slideseq = CreateSpatialPCAObject(
+    counts=sp_count, 
+    location=location, 
+    project = "SpatialPCA", 
+    gene.type="spatial",
+    sparkversion="sparkx",
+    numCores_spark=5, 
+    customGenelist=NULL,
+    min.loctions = 20, 
+    min.features=20
+  )
+  end_time <- Sys.time()
+  T_sparse1 = end_time - start_time
+})
+mem_sparse1
+T_sparse1
+
+# > mem_sparse1
+#   Elapsed_Time_sec Total_RAM_Used_MiB Peak_RAM_Used_MiB
+# 1          147.783              141.9            9963.9
+# > T_sparse1
+# Time difference of 2.463042 mins
+
+mem_sparse1 <- peakRAM({
+  start_time <- Sys.time()
+  slideseq = SpatialPCA_buildKernel(
+    slideseq, 
+    kerneltype="gaussian", 
+    bandwidthtype="Silverman",
+    bandwidth.set.by.user=NULL,
+    sparseKernel=TRUE,
+    sparseKernel_tol=1e-20,
+    sparseKernel_ncore=10
+  )
+  slideseq = SpatialPCA_EstimateLoading(slideseq, fast=TRUE, SpatialPCnum=20)
+  slideseq = SpatialPCA_SpatialPCs(slideseq, fast=TRUE)
+  end_time <- Sys.time()
+  T_sparse1 = end_time - start_time
+})
+mem_sparse1
+T_sparse1
+
+## Selected kernel type is:  gaussian  
+## The bandwidth is:  0.00870514399377566  
+## Calculating sparse kernel matrix
+#   Elapsed_Time_sec Total_RAM_Used_MiB Peak_RAM_Used_MiB
+# 1         1329.051               4539           11439.8
+# > T_sparse1
+# Time difference of 22.15085 mins
+
+# save results
+SpatialPCA_result = list()
+SpatialPCA_result$SpatialPCs  = as.matrix(slideseq@SpatialPCs)
+SpatialPCA_result$normalized_expr  = slideseq@normalized_expr
+SpatialPCA_result$location = slideseq@location
+save(SpatialPCA_result, file = "slideseq_SpatialPCA_result.RData")
+
+
+# -------------------------
+# Obtain clustering result
+# -------------------------
+clusterlabel= louvain_clustering(clusternum=8, latent_dat=slideseq@SpatialPCs, knearest=round(sqrt(dim(slideseq@SpatialPCs)[2])))
+
+# Visualize spatial domains detected by SpatialPCA.
+cbp_spatialpca <- c("lightyellow2", "coral", "lightcyan2" ,"#66C2A5", "cornflowerblue" ,"#FFD92F" ,"#E78AC3", "skyblue1")
+pdf("slideseq_SpatialPCA_cluster8.pdf", width=5, height=5)
+# clusterlabel = SpatialPCA_result$clusterlabel
+p = plot_cluster(
+  legend="right", location=SpatialPCA_result$location, clusterlabel, 
+  pointsize=1, text_size=20, title_in=paste0("SpatialPCA"),color_in=cbp_spatialpca
+)
+p
+dev.off()
