f4ed026624c170509fe310e8e996d5f64ab0856f
Matthias Koenig <konigmatt@googlemail.com>
2023-11-10 23:09:47 +0100

    working on variability

--- src/zia/statistics/lobulus_geometry/plotting/variability.py
+++ src/zia/statistics/lobulus_geometry/plotting/variability.py
@@ -35,7 +35,7 @@ species_colors = {
 def plot_sampling(df_all: pd.DataFrame, species: str):
     """Analyse the effect of sampling"""
 
-    n_samples = 100  # number of repeated samples
+    n_samples = 1000  # number of repeated samples
 
     variables = ["area", "perimeter", "compactness", "minimum_bounding_radius"]
     f, axes = plt.subplots(nrows=4, ncols=1, dpi=300, figsize=(5, 20))
@@ -63,41 +63,90 @@ def plot_sampling(df_all: pd.DataFrame, species: str):
 
             means = np.zeros_like(n_values, dtype=float)
             medians = np.zeros_like(n_values, dtype=float)
-            stds = np.zeros_like(n_values, dtype=float)
+            sds = np.zeros_like(n_values, dtype=float)
+            ses = np.zeros_like(n_values, dtype=float)
+            q05s = np.zeros_like(n_values, dtype=float)
             q25s = np.zeros_like(n_values, dtype=float)
             q75s = np.zeros_like(n_values, dtype=float)
-            qs = [q25s, q75s]
+            q95s = np.zeros_like(n_values, dtype=float)
+            qs = [q05s, q95s]  # [q25s, q75s]
 
-            for k, n in enumerate(n_values):
+
+            for k_lobuli, n_lobuli in enumerate(n_values):
+                all_means = np.zeros(shape=(n_samples,))
                 # sample from values and calculate mean
-                sample: np.ndarray = random.choice(values, size=n, replace=False, p=None)
-                means[k] = sample.mean()
-                medians[k] = np.quantile(sample, q=0.5)
-                stds[k] = sample.std()
-                q25s[k] = np.quantile(sample, q=0.25)
-                q75s[k] = np.quantile(sample, q=0.75)
+                # FIXME: reuse sample for more robust determination
+                sample: np.ndarray = random.choice(values, size=n_lobuli, replace=False,
+                                                   p=None)
+                for k_sample in range(n_samples):
 
-            unit = df[f"{variable}_unit"].unique()[0]
+                    all_means[k_sample] = np.mean(sample)
+
+                means[k_lobuli] = np.mean(all_means)
+                # medians[k] = np.quantile(sample, q=0.5)
+                sds[k_lobuli] = np.std(all_means)
+                ses[k_lobuli] = np.std(all_means) / np.sqrt(n_samples)
+                q05s[k_lobuli] = np.quantile(all_means, q=0.05)
+                q25s[k_lobuli] = np.quantile(all_means, q=0.25)
+                q75s[k_lobuli] = np.quantile(all_means, q=0.75)
+                q95s[k_lobuli] = np.quantile(all_means, q=0.95)
+
             ax = axes[kvar]
+            if kvar == 0:
+                ax.set_title(species.title(), fontweight="bold")
+
+            ax.axhline(y=0.9, color='black', linestyle='--', linewidth=1.0)
+            ax.axhline(y=1.1, color='black', linestyle='--', linewidth=1.0)
             ax.plot(
                 n_values, means,
-                # yerr=qs,
+                # yerr=[means-q05s, q95s-means],
                 marker=markers[k_subject],
                 linestyle="-",
                 color="black",
                 markerfacecolor=species_colors[species],
                 markeredgecolor="black",
-                alpha=0.5,
+                alpha=0.8,
                 label=subject,
             )
+            ax.plot(
+                n_values, q05s,
+                marker="",
+                linestyle="-",
+                color="black",
+                label="Q05" if kvar==0 else "__nolabel__"
+            )
+            ax.plot(
+                n_values, q25s,
+                marker="",
+                linestyle="-",
+                color="darkgrey",
+                label="Q25" if kvar == 0 else "__nolabel__"
+            )
+            ax.plot(
+                n_values, q75s,
+                marker="",
+                linestyle="-",
+                color="darkgrey",
+                label="Q75" if kvar == 0 else "__nolabel__"
+            )
+            ax.plot(
+                n_values, q95s,
+                marker="",
+                linestyle="-",
+                color="darkgrey",
+                label="Q95" if kvar == 0 else "__nolabel__"
+            )
+
             # plot the 1.1 and 0.9 line
-            ax.axhline(y=0.9, color='black', linestyle='--', linewidth=1.0)
-            ax.axhline(y=1.1, color='black', linestyle='--', linewidth=1.0)
             ax.set_ylabel(f"Normalized {variable.title()} (-)")
             ax.set_xlabel(f"Number lobulus (-)")
 
     for ax in axes:
+        ax.set_ylim(0, 2)
+        ax.set_xlim(0.5, 1000)
+        # ax.set_xscale("log")
         ax.legend()
+
 
     f.savefig(f"{species}_variability.png", bbox_inches="tight")
     plt.show()
@@ -111,7 +160,8 @@ if __name__ == "__main__":
     console.print(df)
 
     # plot_df(df)
-    for species in ["mouse", "rat", "pig", "human"]:
+    # for species in ["mouse", "rat", "pig", "human"]:
+    for species in ["mouse"]:
         plot_sampling(
             df_all=df,
             species=species,
