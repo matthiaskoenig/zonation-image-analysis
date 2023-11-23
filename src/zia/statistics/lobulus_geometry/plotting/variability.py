c6862a666d0dc7459733efd9dc053b9a8d9fbdd8
Matthias Koenig <konigmatt@googlemail.com>
2023-11-24 00:24:49 +0100

    calculating number of lobuli

--- src/zia/statistics/lobulus_geometry/plotting/variability.py
+++ src/zia/statistics/lobulus_geometry/plotting/variability.py
@@ -1,10 +1,45 @@
-"""Variability plot for the analysis."""
+"""Variability plot for the analysis.
+
+Bootstrapping analysis
+
+TODO: reuse samples; I.e. same subset of samples for different n;
+bootstrapping samples with replacement.
+It is a straightforward way to derive estimates of standard errors and confidence intervals for complex estimators of the distribution
+Bootstrap 95% confidence intervals?
+
+https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.bootstrap.html
+
+sample size calculator for the mean:
+https://www.statology.org/sample-size-calculator-for-a-mean/
+
+The sample size required to estimate a population mean with a certain level of confidence and a desired margin of error is calculated as:
+Sample size =(zα/2σ/E)2
+
+https://www.omnicalculator.com/statistics/sample-size
+    How accurate should your result be? (margin of error)
+    What level of confidence do you need? (confidence level)
+    What is your initial estimate? (proportion estimate)
+
+https://online.stat.psu.edu/stat506/lesson/2/2.1
+
+n = 1 / (0.1827**2/(1.96**2 * 0.253**2) + 1/1580) ~ 7.3
+n = 1 / (0.1911**2/(1.96**2 * 0.324**2) + 1/664) ~ 10.8
+
+n = 1 / (0.2027**2/(1.96**2 * 0.3779**2) + 1/176) ~ 12.4
+
+"""
 import numpy as np
 import pandas as pd
 from zia.console import console
 from matplotlib import pyplot as plt
 from numpy import random
 
+species_colors = {
+    'mouse': '#77aadd',
+    'rat': '#ee8866',
+    'pig': '#dddddd',
+    'human': '#44bb99'
+}
 
 def plot_df(df: pd.DataFrame) -> None:
     """Test plot to access the data."""
@@ -25,12 +60,7 @@ def plot_df(df: pd.DataFrame) -> None:
 
     plt.show()
 
-species_colors = {
-    'mouse': '#77aadd',
-    'rat': '#ee8866',
-    'pig': '#dddddd',
-    'human': '#44bb99'
-}
+
 
 def plot_sampling(df_all: pd.DataFrame, species: str):
     """Analyse the effect of sampling"""
@@ -151,20 +181,90 @@ def plot_sampling(df_all: pd.DataFrame, species: str):
     f.savefig(f"{species}_variability.png", bbox_inches="tight")
     plt.show()
 
+def plot_n_geometric(df: pd.DataFrame, distances) -> None:
+    """Plot the n required for geometric calculation."""
+    f, axes = plt.subplots(nrows=1, ncols=4, dpi=300, figsize=(20, 5))
+
+    for kplot, attr in enumerate(["perimeter", "area", "compactness", "minimum_bounding_radius"]):
+        ax = axes[kplot]
+        ax.set_title(attr)
+        ax.set_ylabel("Number lobuli")
+        ax.set_xlabel("Margin of error [%]")
+        for species in ["mouse", "rat", "pig", "human"]:
+            df_species = df[(df.species == species) & (df.attr == attr)]
+            # collect data
+            means = []
+            stds = []
+            for d in distances:
+
+                values = df_species[f"n{d}"].values
+                means.append(np.mean(values))
+                stds.append(np.std(values))
+
+            ax.errorbar(
+                x=distances*100,  # conversion to percent
+                y=means,
+                yerr=stds,
+                label=species,
+                color=species_colors[species],
+                linestyle="-", marker="o",
+                markeredgecolor="black"
+            )
+
+        ax.legend()
+
+    plt.show()
+
 
 if __name__ == "__main__":
-    # load dataframe with the information
-    df = pd.read_csv("slide_statistics_df.csv")
-    print(df.columns)
-    print(df["compactness"])
+    # statistics approach
+    # 1. read dataframe
+    data = pd.read_excel("descriptive-stats.xlsx", sheet_name=None)
+    dfs = []
+    for key in [
+        "subject-comparison-human",
+        "subject-comparison-pig",
+        "subject-comparison-rat",
+        "subject-comparison-mouse",
+    ]:
+        df = data[key]
+        species = key.split("-")[-1]
+        df["nominal_var"] = species
+        df.rename(columns={"nominal_var": "species", "group": "subject"}, inplace=True)
+        del df["log"]
+        dfs.append(df)
+
+    df = pd.concat(dfs)
     console.print(df)
 
-    # plot_df(df)
-    # for species in ["mouse", "rat", "pig", "human"]:
-    for species in ["mouse"]:
-        plot_sampling(
-            df_all=df,
-            species=species,
-        )
+
+    # 2. calculate statistics (n_lobule) for 99%, 95% and 90%
+    distances = np.linspace(start=0.05, stop=0.35, num=7)
+    for d in distances:
+        key = f"n{d}"
+
+        # 95%
+        df[key] = 1 / ((df["mean"]*d)** 2 / (1.96 ** 2 * df["std"] ** 2) + 1 / df["n"])
+
+    console.print(df)
+
+    # 3. create plot of the statistics
+    plot_n_geometric(df, distances)
+
+
+    # # bootstrapping approach
+    # # load dataframe with the information
+    # df = pd.read_csv("slide_statistics_df.csv")
+    # print(df.columns)
+    # print(df["compactness"])
+    # console.print(df)
+    #
+    # # plot_df(df)
+    # # for species in ["mouse", "rat", "pig", "human"]:
+    # for species in ["mouse"]:
+    #     plot_sampling(
+    #         df_all=df,
+    #         species=species,
+    #     )
 
 
