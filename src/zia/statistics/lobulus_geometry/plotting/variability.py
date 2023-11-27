aacea8a04ca4b9afbcca8c57bd5347fea30c0a98
Matthias König <konigmatt@googlemail.com>
2023-11-27 17:26:41 +0100

    analysis zonation gradient

--- src/zia/statistics/lobulus_geometry/plotting/variability.py
+++ src/zia/statistics/lobulus_geometry/plotting/variability.py
@@ -37,6 +37,8 @@ from zia.console import console
 from matplotlib import pyplot as plt
 from numpy import random
 
+from zia.statistics.utils.data_provider import SlideStatsProvider
+
 species_colors = {
     'mouse': '#77aadd',
     'rat': '#ee8866',
@@ -48,16 +50,16 @@ species_colors = {
 def plot_n_geometric(df: pd.DataFrame, distances) -> None:
     """Plot the n required for geometric calculation."""
     f: plt.Figure
-    f, axes = plt.subplots(nrows=1, ncols=4, dpi=300, figsize=(17, 4))
-    f.subplots_adjust(wspace=0.3)
+    f, axes = plt.subplots(nrows=1, ncols=4, dpi=300, figsize=(16, 3.5))
+    # f.subplots_adjust(wspace=0.3)
 
+    axes[0].set_ylabel("Number of lobuli [-]", fontdict={"fontweight": "bold", "fontsize": 14})
     for kplot, attr in enumerate(["perimeter", "area", "compactness", "minimum_bounding_radius"]):
 
-
-
         ax = axes[kplot]
-        ax.set_title(attr, fontdict={"fontweight": "bold", "fontsize": 16})
-        ax.set_ylabel("Number of lobuli [-]", fontdict={"fontweight": "bold", "fontsize": 14})
+        title = attr.replace("_", " ")
+        ax.set_title(title, fontdict={"fontweight": "bold", "fontsize": 14})
+
         ax.set_xlabel("Margin of error [%]", fontdict={"fontweight": "bold", "fontsize": 14})
         # ax.set_ylim(top=np.max(df[f"n{distances[0]}"].values))
         ax.set_ylim(top=60)
@@ -91,7 +93,7 @@ def plot_n_geometric(df: pd.DataFrame, distances) -> None:
         ax.legend()
 
     plt.show()
-    f.savefig("n_lobuli.png", bbox_inches="tight")
+    f.savefig("n_lobuli_geometric.png", bbox_inches="tight")
 
 
 def analysis_n_geometric():
@@ -152,105 +154,140 @@ def plot_n_gradient(df: pd.DataFrame):
     plt.style.use("tableau-colorblind10")
 
     protein_order = ["HE", "GS", "CYP1A2", "CYP2D6", "CYP2E1", "CYP3A4"]
-    colors = ["#77AADD", "#EE8866", "#DDDDDD", "#44BB99"]
-
-
-    fig, [ax1, ax2] = plt.subplots(nrows=2, ncols=1, dpi=300, figsize=(5, 10), layout="constrained")
-
-
-    # Simple example:
-    # mouse - GS - 12 bins pv_dist
-    color = species_colors["mouse"]
-
-    species_protein_df = df[(df.species == "mouse") & (df.protein == "GS")]
-    # analysis by individual subjects
-
+    species_order = SlideStatsProvider.species_order
     n_bins = 12
 
-    x_all: Dict[str, np.ndarray] = {}
-    medians_all: Dict[str, np.ndarray] = {}
-    means_all: Dict[str, np.ndarray] = {}
-    stds_all: Dict[str, np.ndarray] = {}
-    ns_all: Dict[str, np.ndarray] = {}
+    fig, axes = plt.subplots(nrows=2, ncols=len(protein_order), dpi=300, figsize=(2*len(protein_order), 2*2), layout="constrained")
 
-    for (subject, protein_df) in species_protein_df.groupby(["subject"]):
+    for species in species_order:
+        for col, protein in enumerate(protein_order):
+            console.print(f"species={species}; protein={protein}")
+            ax1: plt.Axes = axes[0, col]
+            ax2: plt.Axes = axes[1, col]
 
-        lobule_count = len(protein_df.groupby(["subject", "roi", "lobule"]))
-        console.print(f"{lobule_count=}")
+            color = species_colors[species]
+            kwargs = {
+                "marker": "o",
+                "markerfacecolor": color,
+                "markeredgecolor": "black",
+                "linewidth": 1,
+                "markersize": 4,
+                "zorder": 10,
+                "color": color,
+            }
 
-        # binning
-        bins = np.histogram_bin_edges(protein_df["pv_dist"], range=(0, 1), bins=n_bins)
-        binned, bins = pd.cut(protein_df["pv_dist"], bins=bins, retbins=True)
+            species_protein_df = df[(df.species == species) & (df.protein == protein)]
 
-        x = np.zeros(shape=(n_bins-1, ))
-        medians = np.zeros(shape=(n_bins-1, ))
-        means = np.zeros(shape=(n_bins-1, ))
-        stds = np.zeros(shape=(n_bins-1, ))
-        ns = np.zeros(shape=(n_bins-1, ))
+            # analysis by individual subjects
+            x_all: Dict[str, np.ndarray] = {}
+            medians_all: Dict[str, np.ndarray] = {}
+            means_all: Dict[str, np.ndarray] = {}
+            stds_all: Dict[str, np.ndarray] = {}
+            ns_all: Dict[str, np.ndarray] = {}
 
-        for i in range(n_bins - 1):
-            df_bin = protein_df[(protein_df["pv_dist"] > bins[i]) & (protein_df["pv_dist"] <= bins[i + 1])]
-            x[i] = (bins[i] + bins[i + 1]) / 2
-            medians[i] = np.median(df_bin["nintensity"])
-            means[i] = np.mean(df_bin["nintensity"])
-            stds[i] = np.std(df_bin["nintensity"])
+            for (subject, protein_df) in species_protein_df.groupby(["subject"]):
 
-            # TODO: calculate this for the different slides
-            # calculation of the required n at 95% confidence and ME=0.1
-            d = 0.1
-            ns[i] = 1 / ((means[i] * d) ** 2 / (1.96 ** 2 * stds[i] ** 2) + 1 / lobule_count)
+                # FIXME: do analysis via roi;
+                lobule_count = len(protein_df.groupby(["subject", "roi", "lobule"]))
+                console.print(f"{lobule_count=}")
 
-        x_all[subject] = x
-        medians_all[subject] = medians
-        means_all[subject] = means
-        stds_all[subject] = stds
-        ns_all[subject] = ns
+                # binning
+                bins = np.histogram_bin_edges(protein_df["pv_dist"], range=(0, 1), bins=n_bins)
+                binned, bins = pd.cut(protein_df["pv_dist"], bins=bins, retbins=True)
 
-        # plot subject curves
-        ax1.plot(
-            x, medians,
-            marker="o",
-            markerfacecolor=color,
-            markeredgecolor="black",
-            linewidth=1,
-            markersize=4,
-            zorder=10,
-            color="black",
-            alpha=0.5
-        )
+                x = np.zeros(shape=(n_bins, ))
+                medians = np.zeros(shape=(n_bins, ))
+                means = np.zeros(shape=(n_bins, ))
+                stds = np.zeros(shape=(n_bins, ))
+                ns = np.zeros(shape=(n_bins, ))
 
-        ax2.plot(x, ns,
-             marker="o",
-             markerfacecolor=color,
-             markeredgecolor="black",
-             linewidth=1,
-             markersize=4,
-             zorder=10,
-             color="black",
-             alpha=0.5,
-        )
+                # FIXME: ensure complete calculation
+                for i in range(n_bins):  # FIXME: bins
+                    df_bin = protein_df[(protein_df["pv_dist"] > bins[i]) & (protein_df["pv_dist"] <= bins[i + 1])]
+                    x[i] = (0.5 + i) * 1/n_bins
+                    medians[i] = np.median(df_bin["nintensity"])
+                    means[i] = np.mean(df_bin["nintensity"])
+                    stds[i] = np.std(df_bin["nintensity"])
 
-    # plot mean +- sd
+                    # TODO: calculate this for the different slides
+                    # calculation of the required n at 95% confidence and ME=0.1
+                    d = 0.1
+                    ns[i] = 1 / ((means[i] * d) ** 2 / (1.96 ** 2 * stds[i] ** 2) + 1 / lobule_count)
 
+                x_all[subject] = x
+                medians_all[subject] = medians
+                means_all[subject] = means
+                stds_all[subject] = stds
+                ns_all[subject] = ns
 
-    for ax in [ax1, ax2]:
-        # ax.text(x=0.98, y=0.01, s=f"n={lobule_count}", fontsize=10, ha="right", va="bottom", transform=ax.transAxes)
-        ax.set_xticks([])
-        ax.set_xlim(left=0, right=1)
+                # plot subject curves
+                # ax1.plot(
+                #     x, means, #medians,
+                #     alpha=0.5,
+                #     **kwargs
+                # )
+                # ax2.plot(
+                #     x, ns,
+                #     alpha=0.5,
+                #     **kwargs
+                # )
 
-    ax1.set_ylim(bottom=0, top=1.1)
-    ax2.set_ylim(bottom=0)
+            # plot mean +- sd
+            intensity_means = np.mean(np.array([v for v in means_all.values()]), axis=0)
+            intensity_medians = np.median(np.array([v for v in medians_all.values()]), axis=0)
+            intensity_stds = np.std(np.array([v for v in medians_all.values()]), axis=0)
+            intensity_means_stds = np.mean(np.array([v for v in stds_all.values()]), axis=0)
+            ns_means = np.median(np.array([v for v in ns_all.values()]), axis=0)
+            ns_stds = np.std(np.array([v for v in ns_all.values()]), axis=0)
 
-            # ax.set_xticklabels([])
-            # ax.set_yticklabels([])
+            ax1.errorbar(
+                x=x,
+                y=ns_means,
+                yerr=ns_stds,
+                label=species,
+                **kwargs
+            )
 
+            ax2.errorbar(
+                x=x,
+                y=intensity_means,
+                yerr=intensity_means_stds,  # intensity_stds,
+                label=species,
+                **kwargs
+            )
+
+            for ax in [ax1, ax2]:
+                ax.set_xticks([])
+                ax.set_xlim(left=0, right=1)
+
+            ax1.set_ylim(bottom=0, top=100)
+            ax2.set_ylim(bottom=0, top=1.1)
+
+    axes[0, 0].set_ylabel("Number of lobuli [-]", fontsize=9, fontweight="bold")
+    axes[1, 0].set_ylabel("Normalized intensity [-]", fontsize=9, fontweight="bold")
+    for ax in axes.flatten():
+        ax.legend(prop={'size': 6})
+    # axes[0, 0].legend(prop={'size': 6})
+    # axes[1, 0].legend(prop={'size': 6})
+
+    for ax in axes[:-1, :].flatten():
+        ax.set_xticklabels([])
+    for ax in axes[:, 1:].flatten():
+        ax.set_yticklabels([])
+
+    for protein, ax in zip(protein_order, axes[0, :].flatten()):
+        ax.set_title(protein, fontsize=14, fontweight="bold")
+
+    for ax in axes[1, :].flatten():
+        ax.xaxis.set_ticks([0, 1], labels=["PP", "PV"])
 
 
     plt.show()
+    fig.savefig("n_lobuli_gradient.png", bbox_inches="tight")
 
 
 if __name__ == "__main__":
-    # analysis_n_geometric()
+    analysis_n_geometric()
 
     # analysis of the required n for determining the zonation
     # load data for the different regions
